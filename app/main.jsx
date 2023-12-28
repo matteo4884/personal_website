f95b11339811f782abd9b4dad97802a3606027cd
matteo4884 <matteobeu@gmail.com>
2023-12-28 22:10:54 +0100

    backup from movement test

--- app/main.jsx
+++ app/main.jsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-
 export default function Main() {
   return (
     <>
-      <div className="w-[5000px] h-[5000px]">
-        <Image src="/cr7.jpg" width={5000} height={5000} alt="cr7" />
-      </div>
+      <div className="w-[5000px] h-[5000px]">test</div>
     </>
   );
 }
