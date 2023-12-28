f0a3e708c970379c8f1a9a6d613b51eedb11b940
matteo4884 <matteobeu@gmail.com>
2023-12-29 00:42:14 +0100

    new test fix dotted background

--- app/main.jsx
+++ app/main.jsx
@@ -1,4 +1,105 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Main() {
+  const [isScrollingY, setIsScrollingY] = useState("false");
+  const [isScrollingX, setIsScrollingX] = useState("false");
+  const [isMouseOutside, setIsMouseOutside] = useState(false);
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const height = window.innerHeight;
+      const width = window.innerWidth;
+      const mouseY = e.clientY;
+      const mouseX = e.clientX;
+      const triggerX = width / 4;
+      const triggerY = height / 4;
+
+      if (mouseY > height - triggerY) {
+        setIsScrollingY("goDown");
+      } else if (mouseY < triggerY) {
+        setIsScrollingY("goUp");
+      } else {
+        setIsScrollingY("false");
+      }
+
+      if (mouseX > width - triggerX) {
+        setIsScrollingX("goRight");
+      } else if (mouseX < triggerX) {
+        setIsScrollingX("goLeft");
+      } else {
+        setIsScrollingX("false");
+      }
+    };
+
+    const handleMouseLeave = () => {
+      setIsMouseOutside(true);
+    };
+
+    const handleMouseEnter = () => {
+      setIsMouseOutside(false);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document
+      .querySelector("body")
+      .addEventListener("mouseleave", handleMouseLeave);
+    document
+      .querySelector("body")
+      .addEventListener("mouseenter", handleMouseEnter);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document
+        .querySelector("body")
+        .removeEventListener("mouseleave", handleMouseLeave);
+      document
+        .querySelector("body")
+        .removeEventListener("mouseenter", handleMouseEnter);
+    };
+  }, []);
+
+  useEffect(() => {
+    let scrollInterval;
+
+    if (isScrollingY === "goDown") {
+      scrollInterval = setInterval(() => {
+        window.scrollBy(0, 3);
+      }, 10);
+    } else if (isScrollingY === "goUp") {
+      scrollInterval = setInterval(() => {
+        window.scrollBy(0, -3);
+      }, 10);
+    } else {
+      clearInterval(scrollInterval);
+    }
+
+    isMouseOutside && clearInterval(scrollInterval);
+
+    return () => clearInterval(scrollInterval);
+  }, [isScrollingY, isMouseOutside]);
+
+  useEffect(() => {
+    let scrollInterval;
+
+    if (isScrollingX === "goRight") {
+      scrollInterval = setInterval(() => {
+        window.scrollBy(3, 0);
+      }, 10);
+    } else if (isScrollingX === "goLeft") {
+      scrollInterval = setInterval(() => {
+        window.scrollBy(-3, 0);
+      }, 10);
+    } else {
+      clearInterval(scrollInterval);
+    }
+
+    isMouseOutside && clearInterval(scrollInterval);
+
+    return () => clearInterval(scrollInterval);
+  }, [isScrollingX, isMouseOutside]);
+
   return (
     <>
       <div className="w-[5000px] h-[5000px]">test</div>
