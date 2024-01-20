f16c435e2db2b28ec752fa16c14c6dc3c0270b5e
matteo4884 <matteobeu@gmail.com>
2024-01-20 22:24:44 +0100

    push for production

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,15 @@ export default function Home() {
     <>
       <div id="dot_background"></div>
       <Main />
+      <div className="fixed h-screen w-screen bg-black top-0 flex justify-center items-center flex-col">
+        <h1>?</h1>
+        <h1>??</h1>
+        <h1>???</h1>
+        <h1 className="text-white">CAZZO GUARDI</h1>
+        <h1>???</h1>
+        <h1>??</h1>
+        <h1>?</h1>
+      </div>
     </>
   );
 }
