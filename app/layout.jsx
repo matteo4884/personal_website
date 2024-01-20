d93a9680f4e2de3fd0d227e035bbb9309005598b
matteo4884 <matteobeu@gmail.com>
2024-01-20 22:33:33 +0100

    try to fix user scalable

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
       </Head>
       <body className={inter.className}>{children}</body>
