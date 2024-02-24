cf98845fa92fd4530ed4140e8c9e5e780cf2023d
matteo4884 <matteobeu@gmail.com>
2024-02-24 01:46:54 +0100

    bug fixed, change css style

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="fixed w-full top-0 h-[30px] bg-[#fff] flex justify-center items-center">
         <span className="text-[#000] font-semibold">website@matteobeu: ~</span>
       </div>
-      <div className=" pt-[40px] h-screen w-screen bg-black text-[#fff] text-[12px] p-[10px] border-4 border-t-0 border-[#fff]">
+      <div className=" pt-[40px] h-dvh w-screen bg-black text-[#fff] text-[12px] p-[10px] border-4 border-t-0 border-[#fff]">
       
 <pre className="py-[30px]" style={{fontSize: "clamp(4.5px, 1.25vw, 14px)"}}>
 ░▒▓██████████████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░<br />
@@ -22,8 +22,8 @@ export default function Home() {
         <div><span className="font-semibold leading-none">website@matteobeu:&nbsp;</span><span className="leading-none">Webite under code-construction. Launch sequence soon!</span><span id="riga_anim" className="align-middle inline-block h-[12px] w-[5px] bg-[#fff]"></span></div>
                                                               
             <div className="fixed bottom-[10px]"><a href="https://github.com/matteo4884" target="_blank">github</a> | <a href="https://www.instagram.com/matteo_beu/">instagram</a> | <a href="https://www.linkedin.com/in/matteo-beu-73b379238?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">linkedin</a></div>                                                  
-                  
-                                                                                                                                                                               
+
+
       </div>
     </>
   );
