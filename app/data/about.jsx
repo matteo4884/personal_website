455e6d81bb9212dd601e93ceca2b4ff482774794
matteo4884 <matteobeu@gmail.com>
2024-03-21 00:31:39 +0100

    section about new info

--- app/data/about.jsx
+++ app/data/about.jsx
@@ -1,98 +1,129 @@
 export const ABOUT = [
   {
     id: 1,
-    name: "Who am I?",
+    name: "Identity",
     image: "/about/about_whoami.webp",
-    text: "ciaooo",
+    text: (
+      <div>
+        <p>
+          First name: <b>Matteo</b>
+        </p>
+        <p>
+          Last name: <b>Beu</b>
+        </p>
+        <p>
+          Gender: <b>Male</b>
+        </p>
+        <p>
+          Birthdate: <b>09 February 2000</b>
+        </p>
+        <p>
+          Location: <b>Conegliano, Italy</b>
+        </p>
+        <br />
+        <p>
+          I&apos;m not the best at self-description, but at the heart of it,
+          I&apos;m someone deeply passionate about <b>technology</b> and{" "}
+          <b>coding</b>. I&apos;ve always had a forward-looking perspective,
+          drawn to the future and its endless possibilities. For some reason,
+          the past doesn&apos;t captivate me as much; it&apos;s the{" "}
+          <b>future that sparks my curiosity and ambition</b>.
+        </p>
+      </div>
+    ),
   },
   {
     id: 2,
-    name: "Work",
+    name: "Origins",
     image: "/about/about_work.webp",
-    text: "ciaooofdsafasfdasdfdfs",
+    text: (
+      <div>
+        <p>
+          My passion for computing wasn&apos;t sparked in a single moment;
+          it&apos;s been a <b>constant throughout my life</b>. From my earliest
+          memories, I&apos;ve been drawn to technology, starting with video
+          games. This curiosity soon expanded to <b>hardware</b>, where I began
+          exploring the insides of computers. Gradually, my fascination led me
+          to the world of <b>coding</b>, where I started to unravel the magic
+          behind the machines.
+        </p>
+      </div>
+    ),
   },
   {
     id: 3,
-    name: "Work",
+    name: "Philosophy",
     image: "/about/about_work.webp",
-    text: "ciaooofasdfasdf",
+    text: (
+      <div>
+        <p>
+          My thought philosophy might be <b>too crazy</b> to go public, but I
+          firmly believe in <b>science</b>, <b>progress</b>, and{" "}
+          <b>Elon Musk</b>. Let&apos;s colonize the entire galaxy!
+        </p>
+      </div>
+    ),
   },
   {
     id: 4,
-    name: "Work",
+    name: "Hobbies",
     image: "/about/about_work.webp",
-    text: "ciaoooasdfsadfasdfasdf",
+    text: (
+      <div>
+        <ul className="list-disc pl-[18px] uppercase pb-[20px]">
+          <li>
+            <b>coding</b>
+          </li>
+          <li>
+            <b>traveling</b>
+          </li>
+          <li>
+            <b>cooking</b>
+          </li>
+          <li>
+            <b>football</b>
+          </li>
+          <li>
+            <b>videogames</b>
+          </li>
+        </ul>
+        <p>
+          I enjoy traveling the <b>world</b> and seeing all its diversity. I
+          love <b>food and cooking</b>, especially Italian cuisine. I like{" "}
+          <b>football</b> and support AC Milan. When I have some free time, I
+          occasionally play <b>video games</b>. P.S. I&apos;m really good at FC
+        </p>
+      </div>
+    ),
   },
   {
     id: 5,
-    name: "Work",
+    name: "Passions",
     image: "/about/about_work.webp",
     text: "ciaooofadsfasdfasdfasdf",
   },
   {
     id: 6,
-    name: "Work",
+    name: "Inspirations",
     image: "/about/about_work.webp",
     text: "ciaooo fsdasdfasdfsadf",
   },
   {
     id: 7,
-    name: "Work",
+    name: "Travel",
     image: "/about/about_work.webp",
     text: "ciaooo dsfsdfasdfasdfasd",
   },
   {
     id: 8,
-    name: "Work",
+    name: "Growth",
     image: "/about/about_work.webp",
     text: "ciaooo asdf sdfasdfsad",
   },
   {
     id: 9,
-    name: "Work",
+    name: "Future",
     image: "/about/about_work.webp",
     text: "ciaooo dsfsdaf sadf",
-  },
-  {
-    id: 10,
-    name: "Work",
-    image: "/about/about_work.webp",
-    text: "ciaooo dsf sad asd fsad ",
-  },
-  {
-    id: 11,
-    name: "Work",
-    image: "/about/about_work.webp",
-    text: "ciaooo sad sad f s  dsfas ",
-  },
-  {
-    id: 12,
-    name: "Work",
-    image: "/about/about_work.webp",
-    text: "ciaoooasdfsaf sda asd d",
-  },
-  {
-    id: 13,
-    name: "Work",
-    image: "/about/about_work.webp",
-    text: "ciaooo sad fsda fsd fsd",
-  },
-  {
-    id: 14,
-    name: "Work",
-    image: "/about/about_work.webp",
-    text: "ciaooo sadf sad fsad f",
-  },
-  {
-    id: 15,
-    name: "Work",
-    image: "/about/about_work.webp",
-    text: "ciaooo sad fsda sd sad as",
-  },
-  {
-    id: 16,
-    name: "Work",
-    image: "/about/about_work.webp",
-    text: "ciaooo sda fsda ss ",
   },
 ];
--- app/sections/section_5.jsx
+++ app/sections/section_5.jsx
@@ -12,7 +12,7 @@ const redHatDisplay = Red_Hat_Display({
 
 export default function Section5({ setIsStopped }) {
   return (
-    <div className="w-full h-full relative p-12 grid grid-cols-4 grid-rows-4 gap-2">
+    <div className="w-full h-full relative p-12 grid grid-cols-3 grid-rows-3 gap-2">
       <Text />
       {ABOUT.map((el) => {
         return (
@@ -54,7 +54,9 @@ function SectionAbout({ name, image, parag, setIsStopped }) {
         className="group hover:grayscale-0 hover:opacity-100 z-40 transition-all mouse-hover flex justify-center items-center rounded-xl bg-no-repeat bg-cover bg-center grayscale opacity-50"
         style={{ backgroundImage: `url(${image})` }}
       >
-        <h4 className="uppercase transition-all opacity-0 group-hover:opacity-100">
+        <h4
+          className={`${redHatDisplay.className} uppercase transition-all opacity-0 group-hover:opacity-100 font-black bg-[#ddd] --font-xxl text-[#000]`}
+        >
           {name}
         </h4>
       </div>
@@ -65,16 +67,25 @@ function SectionAbout({ name, image, parag, setIsStopped }) {
         className={`z-[102] pointer-events-none transition-all duration-500 flex fixed top-0 left-0 w-screen h-screen justify-center items-center bg-[#000000dc] ${
           isOpen ? "pointer-events-auto" : "opacity-0"
         }`}
+      ></div>
+      <div
+        className={`z-[102] pointer-events-none flex fixed top-0 left-0 w-screen h-screen justify-center items-center`}
       >
-        <div className="rounded-lg bg-[#ddd] h-[75%] w-[50%] --font-m relative p-4 text-[#000]">
+        <div
+          onMouseOver={() => setIsStopped(true)}
+          onMouseLeave={() => setIsStopped(false)}
+          className={`${
+            isOpen ? "pointer-events-auto" : "opacity-0"
+          } rounded-lg transition-all duration-500 bg-[#ddd] tablet:h-max h-max tablet:w-[50%] w-[94%] --font-m relative p-8 text-[#000]`}
+        >
           <RxCross2
             size={25}
             color="#000"
             className="absolute top-2 right-2 mouse-hover"
             onClick={() => setIsOpen(false)}
           />
-          <h1>{name}</h1>
-          <p className="">{parag}</p>
+          <h1 className="--font-xxl text-center uppercase">{name}</h1>
+          <div className="tablet:px-[15%] py-[50px] text-left ">{parag}</div>
         </div>
       </div>
     </>
