46274dfd56b6f8c25b8f60facae98b0ca11ebb3f
matteo4884 <matteobeu@gmail.com>
2024-04-11 23:16:06 +0200

    new animation on video hover

--- app/sections/section_8.jsx
+++ app/sections/section_8.jsx
@@ -1,8 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { TypeAnimation } from "react-type-animation";
+
 export default function Section8() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [isHover, setIsHover] = useState(false);
   return (
     <>
-      <div className="h-full w-full flex justify-center items-center">
-        <div className="flex relative justify-center items-center p-[3%] border-[5px] border-[#ff9900] rounded-3xl">
+      <div className="h-full w-full flex justify-center items-center relative">
+        <a
+          className="flex transition-all justify-center items-center rounded-3xl cursor-pointer"
+          onPointerMove={(e) => {
+            setPosition({ x: e.clientX, y: e.clientY });
+          }}
+          onMouseOver={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
           <video
             width="1200"
             height="auto"
@@ -17,10 +34,34 @@ export default function Section8() {
             <source src="/video/video_computer_v2.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-          {/* <div className="absolute bottom-0">
-            <h4 className="translate-y-[100%]">Just building some sh!t</h4>
-          </div> */}
-        </div>
+          <div
+            className={`fixed left-0 top-8 pointer-events-none ${
+              isHover ? "opacity-1 left-8" : "opacity-0 left-32"
+            }`}
+            style={{
+              transform: `translate(${position.x}px, ${position.y}px)`,
+              transition: "opacity 0.5s ease-in-out, left 0.5s ease-in-out",
+            }}
+          >
+            <h4 className="--font-m p-4 bg-black">
+              <TypeAnimation
+                sequence={[
+                  // Same substring at the start will only be typed once, initially
+                  "Click to read philosophy",
+                  1000,
+                  "Click to read identity",
+                  1000,
+                  "Click to read future",
+                  1000,
+                  "Click to read idea",
+                  1000,
+                ]}
+                speed={50}
+                repeat={Infinity}
+              />
+            </h4>
+          </div>
+        </a>
       </div>
     </>
   );
